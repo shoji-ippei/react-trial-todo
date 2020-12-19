98a8c47d261a56592c2a3e28f024ade7a9be90ed
moepyxxx <sjpy_xx_1007@docomo.ne.jp>
2020-12-19 15:11:29 +0900

    [ADD]: apiを通じてaddtodosができているかテストしたが出来てない

--- client/src/API.ts
+++ client/src/API.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosResponse } from "axios"
-const baseUrl: string = "http://localhost:3000"
+const baseUrl: string = "http://localhost:4000"
 export const getTodos = async (): Promise<AxiosResponse<ApiDataType>> => {
   try {
     const todos: AxiosResponse<ApiDataType> = await axios.get(
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
+import { getTodos, addTodo, updateTodo, deleteTodo } from './API'
 
-function App() {
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<ITodo[]>([])
+
+  const testData: ITodo = {
+    _id: '1',
+    name: 'テストのタスク',
+    description: 'テストのタスクディスクリプション',
+    status: false
+  };
+
+  useEffect(() => {
+    fetchTodos()
+    // レンダリングしたらすぐにダミーデータを追加するよう処理（テスト用）
+    handleSaveTodo(testData)
+    test()
+  }, [])
+
+  const fetchTodos = (): void => {
+    getTodos()
+    .then(({ data: { todos } }: ITodo[] | any) => {
+      setTodos(todos)
+    })
+    .catch((err: Error) => console.log(err))
+  }
+
+  const handleSaveTodo = (data: ITodo): void => {
+    addTodo(data)
+      .then(({ status, data }) => {
+        if (status !== 201) {
+          throw new Error("Error! Todo not saved")
+        }
+        setTodos(data.todos)
+        console.log(todos)
+      })
+      .catch(err => console.log(err))
+  }
+
+  // 正常に動いているか確認する用
+  const test = (): void => {
+    console.log('hello')
+  }
+
   return (
-    <div className="App">
-      <p>hello, world</p>
-    </div>
-  );
+    <main className='App'>
+      <h1>タスク管理アプリ</h1>
+      <ul>
+        {todos.map((todo: ITodo) => (
+          <li key={todo._id}>{todo.name}</li>
+        ))}
+      </ul>
+    </main>
+  )
 }
 
-export default App;
+export default App
