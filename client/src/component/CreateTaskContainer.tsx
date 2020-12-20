da0b184638995374faa6550750c1bad671ed6071
moepyxxx <sjpy_xx_1007@docomo.ne.jp>
2020-12-20 19:45:23 +0900

    [ADD]: CreateTaskContainerへjsx追加

--- client/src/component/CreateTaskContainer.tsx
+++ client/src/component/CreateTaskContainer.tsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 const CreateTaskContainer: React.FC = () => {
   return (
-    <div>
-      CreateTaskContainer
+    <div className="p-createtask">
+      <div className="p-createtask__ttl">
+        <p>タスクの追加</p>
+      </div>
+      <div className="p-createtask__form">
+        <div className="p-createtask__form_item">
+          <input type="text" name="title" value="" />
+        </div>
+        <div className="p-createtask__form_item">
+          <textarea name="description"></textarea>
+        </div>
+        <div className="p-createtask__form_item">
+          <button>タスクの追加</button>
+        </div>
+      </div>
     </div>
   )
 }
